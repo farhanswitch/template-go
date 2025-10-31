58e51b8bc60a81664bd957a6c45e20fcbd000d14
Muhammad Farhan <muhammad_farhan@outlook.co.id>
2025-10-31 16:45:46 +0700

    Add main.go as a starting point

--- connections/postgres.connection.go
+++ connections/postgres.connection.go
@@ -13,7 +13,7 @@ import (
 var dbInstancePostgres *sql.DB
 
 func DbPostgres() *sql.DB {
-	if dbInstance == nil {
+	if dbInstancePostgres == nil {
 		dbConf := configs.GetConfig().DbPostgres
 		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbConf.Host, dbConf.Port, dbConf.User, dbConf.Pass, dbConf.Database)
 
--- main.go
+++ main.go
@@ -1,1 +1,63 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"template/configs"
+	"template/connections"
+
+	chi "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func main() {
+	initModules()
+	router := chi.NewRouter()
+
+	initPlugins(router)
+	internalModules(router)
+	router.Options("/*", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Header.Get("Origin"))
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	router.Get("/health-check", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"healthy"}`))
+	}))
+	var port uint16 = configs.GetConfig().Service.Port
+	var host string = configs.GetConfig().Service.Host
+	log.Printf("Server running on  %s:%d", host, port)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router)
+}
+
+func initModules() {
+	configs.InitModule(".env")
+	connections.DbMySQL()
+	connections.DbPostgres()
+	connections.ConnectRedis()
+}
+func internalModules(router *chi.Mux) {
+	// users.InitModule(router)
+	// files.InitModule(router)
+	// markets.InitModule(router)
+	// stores.InitModule(router)
+}
+func initPlugins(router *chi.Mux) {
+	// Middleware CORS
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link", "Xrf-Token"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	router.Use(middleware.Recoverer)
+
+}
