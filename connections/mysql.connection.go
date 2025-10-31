d81bf70046357345032ce515af69a81c10fb2298
Muhammad Farhan <muhammad_farhan@outlook.co.id>
2025-10-31 16:26:04 +0700

    Create file connection mysql

---
+++ connections/mysql.connection.go
@@ -0,0 +1,41 @@
+package connections
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"template/configs"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var dbInstance *sql.DB
+
+func DbMySQL() *sql.DB {
+	if dbInstance == nil {
+		dbConf := configs.GetConfig().DbMySql
+		dbPort := dbConf.Port
+		config := mysql.Config{
+			User:                 dbConf.User,
+			Passwd:               dbConf.Pass,
+			Net:                  "tcp",
+			Addr:                 fmt.Sprintf("%s:%d", dbConf.Host, dbPort),
+			DBName:               dbConf.Database,
+			AllowNativePasswords: true,
+		}
+
+		db, err := sql.Open("mysql", config.FormatDSN())
+		if err != nil {
+			log.Fatalf("Cannot connect to Database.\nError: %s", err.Error())
+		}
+		dbInstance = db
+		pingErr := db.Ping()
+		if pingErr != nil {
+			log.Fatalf("Cannot ping the database. Error: %s", pingErr.Error())
+		}
+
+		log.Println("[MYSQL] Database connected!")
+	}
+	return dbInstance
+}
